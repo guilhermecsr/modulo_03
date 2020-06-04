046dcc05334017c97321af1d93d364d768ac5d82
guilhermecsr <guicsramos@gmail.com>
2020-06-04 15:18:01 -0300

    rotas e REST

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'welcome/index'
+  # get '/inicio', to: 'welcome#index'
+
   resources :coins
+  #get '/coins', to: 'coins#index'
+
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
