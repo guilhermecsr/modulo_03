1bdfa8addfb78e7bd3f2b25a20be8457686406b2
guilhermecsr <guicsramos@gmail.com>
2020-06-08 14:43:31 -0300

    find or create by

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,47 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Criando as moedas....."
+spinner = TTY::Spinner.new("[:spinner] Carregando moedas ...", format: :dots_4)
+spinner.auto_spin
 
-Coin.create!(
-        description: "Bitcoin",
-        acronym: "BTC",
-        url_image: " https://img2.gratispng.com/20180330/wgw/kisspng-bitcoin-cryptocurrency-monero-initial-coin-offerin-bitcoin-5abdfe6b87dad3.2673609815224008755565.jpg "
-)
+coins = [{
+             description: "Bitcoin",
+             acronym: "BTC",
+             url_image: "https://img2.gratispng.com/20180330/wgw/kisspng-bitcoin-cryptocurrency-monero-initial-coin-offerin-bitcoin-5abdfe6b87dad3.2673609815224008755565.jpg"
+         },
+         {
+             description: "Ethereum",
+             acronym: "ETH",
+             url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/1027.png"
+         },
+         {
+             description: "Dash",
+             acronym: "DASH",
+             url_image: "https://i7.pngflow.com/pngimage/37/123/png-dash-bitcoin-cryptocurrency-digital-currency-logo-bitcoin-blue-angle-text-rectangle-clipart.png"
+         }]
 
-Coin.create!(
-    description: "Ethereum",
-    acronym: "ETH",
-    url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/1027.png "
-)
+coins.each do |coin|
+     sleep(1)
+     Coin.find_or_create_by!(coin)
+end
 
-Coin.create!(
-    description: "Dash",
-    acronym: "DASH",
-    url_image: "https://i7.pngflow.com/pngimage/37/123/png-dash-bitcoin-cryptocurrency-digital-currency-logo-bitcoin-blue-angle-text-rectangle-clipart.png "
-)
 
-puts "Moedas criadas com sucesso!"
+# Coin.create!(
+#     [{
+#          description: "Bitcoin",
+#          acronym: "BTC",
+#          url_image: "https://img2.gratispng.com/20180330/wgw/kisspng-bitcoin-cryptocurrency-monero-initial-coin-offerin-bitcoin-5abdfe6b87dad3.2673609815224008755565.jpg"
+#      },
+#      {
+#          description: "Ethereum",
+#          acronym: "ETH",
+#          url_image: "https://s2.coinmarketcap.com/static/img/coins/200x200/1027.png"
+#      },
+#      {
+#          description: "Dash",
+#          acronym: "DASH",
+#          url_image: "https://i7.pngflow.com/pngimage/37/123/png-dash-bitcoin-cryptocurrency-digital-currency-logo-bitcoin-blue-angle-text-rectangle-clipart.png"
+#      }]
+# )
+
+spinner.success("(Concluido!)")
