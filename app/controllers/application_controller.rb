2daab58d1824f8d8df831c07ece0571e2f293281
guilhermecsr <guicsramos@gmail.com>
2020-06-10 17:16:20 -0300

    mudanca de idiomas em tempo de aplicacao e correcao de erros

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale]
+      cookies[:locale] = params[:locale]
+    end
+
+    if cookies[:locale]
+      if I18n.locale != cookies[:locale]
+        I18n.locale = cookies[:locale]
+      end
+    end
+    # locale = params[:locale] || I18n.default_locale
+    # I18n.with_locale(locale, &action)
+  end
 end
